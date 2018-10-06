cb00ef0166b37cf2c3532c47f5f4f07f86398c91
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-10-06 20:37:29 -0300

    refactor main to use vgg model

--- main.py
+++ main.py
@@ -1,70 +1,25 @@
-from math import pi
-
-import torch
-
 import numpy as np
-from torch.optim import Adam
+import torch
+from torch.optim import Adam, SGD
 from torch.utils.data import DataLoader
 
 import settings
+import torch.nn.functional as F
 from data_load import load_mpii_dataframes
 from device_dataset import DeviceDataset
 from model import Net
+from model_vgg import NetVgg
 from mpii_dataset import MpiiDataset
 from mpii_normalizing_dataset import MpiiNormalizingDataset
+from resize_dataset import ResizeDataset
 from tensor_dataset import TensorDataset
-
-
-def loss_func(z, mean, var):
-    t1 = 2 * pi * var
-    t1 = torch.pow(t1, -1 / 2)
-    t1 = torch.log(t1)
-
-    t2 = z - mean
-    t2 = torch.pow(t2, 2)
-    t2 = - t2
-    t2 = t2 / (2 * var)
-
-    loss = t1 + t2
-    # loss = torch.exp(loss)
-    # loss = torch.log(loss)
-    loss = torch.mean(loss)
-    loss = -loss
-
-    return loss
-
-
-def evaluate(model, x, x_axis, y_axis):
-    mean_x, var_x, mean_y, var_y = model(x)
-
-    x_axis = x_axis.cpu().detach().numpy()
-    y_axis = y_axis.cpu().detach().numpy()
-
-    mean_x = mean_x.cpu().detach().numpy()
-    mean_y = mean_y.cpu().detach().numpy()
-    var_x = var_x.cpu().detach().numpy()
-    var_y = var_y.cpu().detach().numpy()
-
-    x_pred = np.zeros_like(mean_x)
-    y_pred = np.zeros_like(mean_y)
-
-    n_samples = 20
-    for i in range(n_samples):
-        x_pred[:] += np.random.normal(mean_x, np.power(var_x, 2))
-        y_pred[:] += np.random.normal(mean_y, np.power(var_y, 2))
-    x_pred /= n_samples
-    y_pred /= n_samples
-
-    diff_x = np.mean(np.abs(x_axis - x_pred))
-    diff_y = np.mean(np.abs(y_axis - y_pred))
-
-    return diff_x, diff_y
 
 
 def train(data_frame, params):
     device = torch.device('cuda')
 
     dataset = MpiiDataset(data_frame)
+    dataset = ResizeDataset(dataset)
     dataset = MpiiNormalizingDataset(dataset,
                                      mean=(0.33320256,
                                            0.35879958,
@@ -77,18 +32,21 @@ def train(data_frame, params):
 
     loader = DataLoader(dataset=dataset, batch_size=settings.BATCH_SIZE)
 
-    model = Net().to(device)
+    model = NetVgg().to(device)
 
-    optimizer = Adam(model.parameters(), lr=params['learning_rate'])
+    optimizer = SGD(model.parameters(), lr=params['learning_rate'], momentum=0.9)
 
     epochs = params['epochs']
     for epoch in range(epochs):
         losses = []
-        for i, (x, m, x_axis, y_axis) in enumerate(loader):
-            mean_x, var_x, mean_y, var_y = model(x)
-            x_loss = loss_func(x_axis, mean=mean_x, var=var_x)
-            y_loss = loss_func(y_axis, mean=mean_y, var=var_y)
-            loss = x_loss + y_loss
+        for i, (x, m, x_true, y_true) in enumerate(loader):
+            x_pred, y_pred = model(x)
+
+            # loss = F.mse_loss(x_pred, x_true) + F.mse_loss(y_pred, y_true)
+            # loss = torch.mean(loss)
+            loss_x = torch.mean(torch.abs(x_true - x_pred))
+            loss_y = torch.mean(torch.abs(y_true - y_pred))
+            loss = loss_x + loss_y
 
             losses.append(float(loss))
 
@@ -97,17 +55,12 @@ def train(data_frame, params):
             optimizer.step()
 
             if i % 50 == 0:
-                print('training epoch {}/{} batch {}/{} loss {}'.format(epoch + 1, epochs, i + 1, len(loader),
-                                                                        float(loss)))
-                diff_x, diff_y = evaluate(model, x, x_axis, y_axis)
-
-                print('diff_x', diff_x)
-                print('diff_y', diff_y)
+                print('training epoch {}/{} batch {}/{} loss {}'.format(epoch + 1, epochs, i + 1, len(loader), float(loss)))
 
 
 if __name__ == '__main__':
     df_train, df_valid, df_test = load_mpii_dataframes()
     train(df_train, params={
-        'learning_rate': 1e-3,
-        'epochs': 5
+        'learning_rate': 0.001,
+        'epochs': 999999999999
     })
