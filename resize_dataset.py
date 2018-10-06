b9dccbd0d95f0d3778d6db342480d1eb9a1158a4
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-10-06 20:37:07 -0300

    adds resize dataset decorator

---
+++ resize_dataset.py
@@ -0,0 +1,16 @@
+import torch.utils.data as data
+import cv2
+
+
+class ResizeDataset(data.Dataset):
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __getitem__(self, i):
+        x, m, x_axis, y_axis = self.dataset[i]
+        x = cv2.resize(x, dsize=(255, 255))
+        return x, m, x_axis, y_axis
+
+    def __len__(self):
+        return len(self.dataset)
