025a0455110a993eff9d6829c4be96f5a45b2e5a
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-10-13 22:45:44 -0300

    prepare dataset refactor

--- prepare_mpii_face_gaze.py
+++ prepare_mpii_face_gaze.py
@@ -3,7 +3,7 @@ from collections import deque
 import cv2
 import numpy as np
 from os import listdir
-from os.path import join, isdir, splitext
+from os.path import join, isdir, splitext, isfile
 
 from scipy.io import loadmat
 
@@ -35,6 +35,18 @@ if __name__ == '__main__':
                     # noinspection PyRedeclaration
                     image_filename, *tail = deque(line.split())
                     image_fullname = join(participant_dir_fullname, image_filename)
+
+                    # noinspection PyRedeclaration
+                    day, imagename = image_filename.split('/')
+                    imagename, _ = splitext(imagename)
+
+                    imagename_new = '{}_{}_{}.jpg'.format(participant_dir, day, imagename)
+                    imagename_new = join(mpii_dir_prepared, imagename_new)
+
+                    if isfile(imagename_new):
+                        print('skipping', imagename_new)
+                        continue
+
                     target_x, *tail = tail
                     target_y, *tail = tail
                     target_x = int(target_x)
@@ -42,20 +54,18 @@ if __name__ == '__main__':
                     target_x /= screen_w
                     target_y /= screen_h
 
-                    # noinspection PyRedeclaration
-                    day, imagename = image_filename.split('/')
-                    imagename, _ = splitext(imagename)
+                    print('starting participant {} day {} img {}'.format(participant_dir, day, imagename))
 
-                    # noinspection PyRedeclaration
-                    img = cv2.imread(image_fullname)
-                    img = extract_face(img)
-                    img = cv2.resize(img, dsize=(448, 448))
+                    try:
+                        # noinspection PyRedeclaration
+                        img = cv2.imread(image_fullname)
+                        img = extract_face(img)
+                        img = cv2.resize(img, dsize=(448, 448))
 
-                    imagename_new = '{}_{}_{}.jpg'.format(participant_dir, day, imagename)
-                    imagename_new = join(mpii_dir_prepared, imagename_new)
+                        cv2.imwrite(imagename_new, img)
 
-                    cv2.imwrite(imagename_new, img)
-
-                    f.write('{},{},{}\n'.format(imagename_new, target_x, target_y))
-
-                    print('finished participant {} day {} img {}'.format(participant_dir, day, imagename))
+                        f.write('{},{},{}\n'.format(imagename_new, target_x, target_y))
+                    except AssertionError as e:
+                        print('assert error')
+                    except Exception as e:
+                        print(str(e))
